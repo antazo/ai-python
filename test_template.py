32a13c22868f624b08354cf488f77c90b7bc3ac5
AlexAN <alex.antazo@msn.com>
2024-10-10 11:32:33 +0200

    Refactor to use unittest

--- test_template.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from py.template import interstitial
-
-class TestTemplate(unittest.TestCase):
-    def test_interstitial(self):
-        self.assertEqual(interstitial(), "Hello, World!")
-
-if __name__ == '__main__':
-    unittest.main()
