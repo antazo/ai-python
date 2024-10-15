361c00cd78222b567bb3d6c0a2922d39f8b69e10
AlexAN <alex.antazo@msn.com>
2024-10-15 12:52:45 +0200

    Add /game (rock, paper, scissors), and /foobar endpoints

--- py/app.py
+++ py/app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import json
+import random
 from flask import Flask, redirect, url_for, request, render_template, session
 
 app = Flask(__name__)
@@ -10,6 +11,8 @@ def start():
     body = f"<h1>Hello {name}!</h1>"
     body += f"<p>This is created with Flask. These are the endpoints with examples:</p>"
     body += f"<p><a href=\"translator\">/translator</a></p>"
+    body += f"<p><a href=\"game\">/game</a></p>"
+    body += f"<p><a href=\"foobar\">/foobar</a></p>"
     body += f"<p><a href=\"hello?name=Alex\">/hello?name=Alex</a></p>"
     body += f"<p><a href=\"bye?name=Alex\">/bye?name=Alex</a></p>"
     body += f"<p><a href=\"planet_distances\">/planet_distances</a></p>"
@@ -59,10 +62,68 @@ def generate_report():
     """
     return output
 
+@app.route('/game')
+def game():
+    class GameRockPaperScissors:
+        def __init__(self):
+            self.player = None
+            self.enemy = None
+
+        def play(self):
+            if self.player == self.enemy:
+                return "It's a tie!"
+            elif self.player == "rock":
+                if self.enemy == "scissors":
+                    return "You win!"
+                else:
+                    return "You lose!"
+            elif self.player == "scissors":
+                if self.enemy == "paper":
+                    return "You win!"
+                else:
+                    return "You lose!"
+            elif self.player == "paper":
+                if self.enemy == "rock":
+                    return "You win!"
+                else:
+                    return "You lose!"
+            else:
+                return "Invalid input!"
+
+    game = GameRockPaperScissors()
+    body = f"<h1>/game</h1>"
+    body += f"<p>Choose rock, paper, or scissors:</p>"
+    body += f"<p><a href=\"game?choice=rock\">rock</a></p>"
+    body += f"<p><a href=\"game?choice=paper\">paper</a></p>"
+    body += f"<p><a href=\"game?choice=scissors\">scissors</a></p>"
+    choicePlayer = request.args.get('choice')
+    choiceEnemy = random.choice(["rock", "paper", "scissors"])
+    if choicePlayer:
+        game.player = choicePlayer
+        game.enemy = choiceEnemy
+        result = game.play()
+        body += f"<p>Enemy chooses <b>{choiceEnemy}</b>: {result}</p>"
+    return body
+
 @app.route('/foobar')
 def foobar():
-    body = f"<h1>foobar</h1>"
 
+    class Foo:
+        def __init__(self):
+            self.a = 1
+            self.b = 2
+
+    class Bar:
+        def __init__(self):
+            self.c = 3
+            self.d = 4
+
+    foo = Foo()
+    bar = Bar()
+
+    body = f"<h1>/foobar</h1>"
+    body += f"foo.a: {foo.a}<br>"
+    body += f"bar.c: {bar.c}<br>"
     return body
 
 if __name__ == '__main__':
