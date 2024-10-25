ac361975f731942ea85f9af61c57feb2415fe3cd
AlexAN <alex.antazo@msn.com>
2024-10-25 22:58:12 +0200

    HTML code to templates

--- app.py
+++ app.py
@@ -22,21 +22,12 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 @app.route('/')
-def start():
+def home():
     name = request.args.get('name', 'visitor')
-    html_content = f"<h1>Hello {name}!</h1>"
-    html_content += f"<p>This is created with Flask. These are the endpoints with examples:</p>"
-    html_content += f"<p><a href=\"translator\">/translator</a>: uses Azure AI Translator cognitive service.</p>"
-    html_content += f"<p><a href=\"game\">/game</a>: play rock, paper, scissors, lizard, Spock!</p>"
-    html_content += f"<h1>Tests:</h1>"
-    html_content += f"<p><a href=\"foobar\">/foobar</a>: lorem ipsum</p>"
-    html_content += f"<p><a href=\"hello?name=Alex\">/hello?name=Alex</a>: your favorite 'Hello World'.</p>"
-    html_content += f"<p><a href=\"bye?name=Torped@\">/bye?name=Torped@</a>: idem, to play with serialization.</p>"
-    html_content += f"<p><a href=\"generate_report?main_tank=80&external_tank=80&hydrogen_tank=75\">/generate_report?main_tank=80&external_tank=80&hydrogen_tank=75</a></p>"
-    html_content += f"<h1>REST:</h1>"
-    html_content += f"<p><a href=\"planet_distances\">/planet_distances</a>: distances of all the planets from the Solar System to the Earth.</p>"
-    html_content += f"<p>[<a href=\"swagger\">swagger</a> page]</p>"
-    return html_content
+    return render_template(
+        'home.html',
+        name=name
+    )
 
 @app.route('/translator', methods=['GET'])
 def translator():
@@ -165,60 +156,23 @@ def game():
 
     game = GameRockPaperScissors()
     
-    
-    html_content = f"""
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>/game</title>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"></script>
-        <style>
-            .choose {{
-                text-align: center;
-                font-size: 36px;
-            }}
-            .result {{
-                text-align: center;
-                font-size: 24px;
-            }}
-        </style>
-    </head>
-    <body>
-        <div class="choose">
-    """
-    html_content += f"<p>Choose:</p>"
-    for key in game.switcher.keys():
-        html_content += f"<p><a href=\"game?choice={key}\">{key}</a></p>"
+    choices = list(game.switcher.keys())
+
     choicePlayer = request.args.get('choice').lower() if request.args.get('choice') in list(game.switcher.keys()) else None
     choiceEnemy = random.choice(list(game.switcher.keys()))
-    html_content += f"</div>"
     
+    result = None
     if choicePlayer:
         game.player = choicePlayer
         game.enemy = choiceEnemy
         result = game.play()
-        html_content += f"""
-            <div class="result">
-                <p>Enemy chooses <b>{choiceEnemy}</b>...</p>
-                <p>Yours is <b>{choicePlayer}</b>: {result}</p>
-                <button onclick="window.location.href='/game?choice={choicePlayer}'">Replay?</button>
-            </div>
-        """
-    html_content += f"""
-        <script>
-            gsap.from(".choose", {{duration: 1, opacity: 0, y: -50}});
-            gsap.from(".result", {{duration: 1, opacity: 0, y: -50}});
-            gsap.from(".result", {{duration: 1, x: -100}});
-            gsap.from(".result", {{duration: 1, scale: 0}});
-            gsap.from(".result", {{duration: 1, rotation: 360}});
-            gsap.from(".result", {{duration: 1, y: -100, ease: "bounce"}});
-        </script>
-    </body>
-    </html>
-    """
-    return render_template_string(html_content)
+    return render_template(
+        'rockpaperscissors.html',
+        choices=choices,
+        choicePlayer=choicePlayer,
+        choiceEnemy=choiceEnemy,
+        result=result
+    )
 
 @app.route('/foobar')
 def foobar():
